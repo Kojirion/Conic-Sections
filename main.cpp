a2d56838cb25125ce6e831e9cce46000fb5c2c2c
Kojirion <kojirion@gmail.com>
2013-11-02 02:36:43 +0000

    some camera movement + drawing with depth testing

--- main.cpp
+++ main.cpp
@@ -4,6 +4,13 @@
 #include <Thor/Input/EventSystem.hpp>
 #include "Cone.hpp"
 #include "Conic.hpp"
+//#include <Thor/Vectors/VectorAlgebra3D.hpp>
+
+struct Camera{
+    sf::Vector3f eye;
+    sf::Vector3f lookAt;
+    sf::Vector3f up;
+};
 
 int main()
 {
@@ -17,7 +24,10 @@ int main()
     thor::ActionMap<std::string>::CallbackSystem system;
 
     actions["close"] = thor::Action(sf::Event::Closed);
-    actions["right"] = thor::Action(sf::Keyboard::Right);
+    actions["strafeRight"] = thor::Action(sf::Keyboard::Right);
+    actions["strafeLeft" ] = thor::Action(sf::Keyboard::Left );
+    actions["strafeUp"   ] = thor::Action(sf::Keyboard::Up   );
+    actions["strafeDown" ] = thor::Action(sf::Keyboard::Down );
 
 
     sf::Window window(sf::VideoMode(width, height), "Conic sections", sf::Style::Default, sf::ContextSettings(32));
@@ -25,26 +35,42 @@ int main()
 
     system.connect("close", std::bind(&sf::Window::close, &window));
 
-    system.connect("right", [](thor::ActionContext<std::string> context){
+    Camera camera{{0,0,200.}, {0,0,0}, {0,1,0}};
 
-    });
+    typedef thor::ActionContext<std::string> ActionContext;
+
+    system.connect("strafeRight", [&camera](ActionContext){ camera.eye.x += 10.f; });
+    system.connect("strafeLeft" , [&camera](ActionContext){ camera.eye.x -= 10.f; });
+    system.connect("strafeUp"   , [&camera](ActionContext){ camera.eye.y += 10.f; });
+    system.connect("strafeDown" , [&camera](ActionContext){ camera.eye.y -= 10.f; });
 
 
+    glEnable (GL_DEPTH_TEST);
     glViewport(0, 0, width, height);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluPerspective(120, width / float(height), .1, 500);
-    glMatrixMode(GL_MODELVIEW);
-    gluLookAt(0,0,200.,0,0,0,0,1,0);
+
 
     while (window.isOpen()){
         actions.update(window);
         actions.invokeCallbacks(system, &window);
 
+        //update here
+        glMatrixMode(GL_MODELVIEW);
+        glLoadIdentity();
+        gluLookAt(camera.eye.x, camera.eye.y, camera.eye.z,
+                  camera.lookAt.x, camera.lookAt.y, camera.lookAt.z,
+                  camera.up.x, camera.up.y, camera.up.z);
+
+        //draw here
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         cone.draw();
         plane.draw();
+
+
 
         window.display();
 
